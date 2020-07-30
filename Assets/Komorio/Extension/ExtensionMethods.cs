b0b3e8b456c3161852f6f21e0f3b42eb86bc0aa9
Komorio <twilivez@gmail.com>
2020-07-31 03:45:05 +0900

    [ADD] Log 메소드 및 Unity Events 추가

---
+++ Assets/Komorio/Extension/ExtensionMethods.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class ExtensionMethods
+{
+    public static void Log(object value){
+        #if UNITY_EDITOR
+        Debug.Log(value.ToString());
+        #endif
+    }
+}
---
+++ Assets/Komorio/Extension/UnityEvents.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.Events;
+using UnityEngine;
+
+[Serializable]
+public class Event : UnityEvent { }
+
+[Serializable]
+public class Event<T> : UnityEvent<T> { }
+
+[Serializable]
+public class Event<T1, T2> : UnityEvent<T1, T2> { }
+
+[Serializable]
+public class Event<T1, T2, T3> : UnityEvent<T1, T2, T3> { }
+
+[Serializable]
+public class Event<T1, T2, T3, T4> : UnityEvent<T1, T2, T3, T4> { }
