3c1c5145fe74dddda29c47827d8a203dd72bb117
Komorio <twilivez@gmail.com>
2020-08-04 11:57:06 +0900

    [UPDATE] UnityEvents 내역 수정

--- Assets/Komorio/Extension/UnityEvents.cs
+++ Assets/Komorio/Extension/UnityEvents.cs
@@ -4,17 +4,44 @@ using System.Collections.Generic;
 using UnityEngine.Events;
 using UnityEngine;
 
-[Serializable]
-public class Event : UnityEvent { }
+/// <summary>
+/// This script can only be used in 2020 or later versions. 
+/// if you want be used in other versions, you must use "EventTools.FormerEvent" 
+/// </summary>
+namespace EventTools.Event{
+    [Serializable]
+    public class Event : UnityEvent { }
 
-[Serializable]
-public class Event<T> : UnityEvent<T> { }
+    [Serializable]
+    public class Event<T> : UnityEvent<T> { }
 
-[Serializable]
-public class Event<T1, T2> : UnityEvent<T1, T2> { }
+    [Serializable]
+    public class Event<T1, T2> : UnityEvent<T1, T2> { }
 
-[Serializable]
-public class Event<T1, T2, T3> : UnityEvent<T1, T2, T3> { }
+    [Serializable]
+    public class Event<T1, T2, T3> : UnityEvent<T1, T2, T3> { }
 
-[Serializable]
-public class Event<T1, T2, T3, T4> : UnityEvent<T1, T2, T3, T4> { }
+    [Serializable]
+    public class Event<T1, T2, T3, T4> : UnityEvent<T1, T2, T3, T4> { }
+}
+
+/// <summary>
+/// This script can be used in 2020 and than previous versions.
+/// but, if you use 2020 or later version, I recommend you to use EventTools.Events 
+/// </summary>
+namespace EventTools.FormerEvent{
+    [Serializable]
+    public class VoidEvent : UnityEvent { }
+
+    [Serializable]
+    public class IntEvent : UnityEvent<int> { }
+
+    [Serializable]
+    public class FloatEvent : UnityEvent<float> { }
+
+    [Serializable]
+    public class StringEvent : UnityEvent<string> { }
+
+    [Serializable]
+    public class DoubleIntEvent : UnityEvent<int, int> { }
+}
