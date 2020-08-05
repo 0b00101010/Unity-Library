2026a4c34a2ce386dafaed2ebec93812869b1ed2
Komorio <twilivez@gmail.com>
2020-08-05 12:08:57 +0900

    [UPDATE] ExtensionMethods 수정

--- Assets/Komorio/Extension/ExtensionMethods.cs
+++ Assets/Komorio/Extension/ExtensionMethods.cs
@@ -2,9 +2,12 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// Extension Method is a method that can be used in a particular data type.
+/// </summary>
 public static class ExtensionMethods
 {
-    public static void Log(object value){
+    public static void Log(this object value){
         #if UNITY_EDITOR
         Debug.Log(value.ToString());
         #endif
