7807f238f11e69a041859648c386c6c702c36d21
Komorio <twilivez@gmail.com>
2020-08-10 04:53:13 +0900

    [ADD] Extension Methods 분할 및 Graphics color, Transform position 변경하는 기능 추가

--- Assets/01.Scripts/Komorio/Extension/SystemExtensions.cs
+++ Assets/01.Scripts/Komorio/Extension/SystemExtensions.cs
@@ -1,15 +1,14 @@
-﻿using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 /// <summary>
 /// Extension Method is a method that can be used in a particular data type.
 /// </summary>
-public static class ExtensionMethods
-{
+public static class SystemExtensions{
     public static void Log(this object value){
         #if UNITY_EDITOR
         Debug.Log(value.ToString());
         #endif
     }
+
+    
 }
