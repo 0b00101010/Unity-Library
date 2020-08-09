819d4bf08aef67d20522c30d8ea849ad805908c9
Komorio <twilivez@gmail.com>
2020-08-09 17:54:44 +0900

    [UPDATE] 파일 위치 이동

---
+++ Komorio/Extension/ExtensionMethods.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Extension Method is a method that can be used in a particular data type.
+/// </summary>
+public static class ExtensionMethods
+{
+    public static void Log(this object value){
+        #if UNITY_EDITOR
+        Debug.Log(value.ToString());
+        #endif
+    }
+}
