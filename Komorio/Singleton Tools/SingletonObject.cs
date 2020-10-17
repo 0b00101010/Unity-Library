572ba69623a9b222a4e88a1ff8c91cefa31800df
Jeon Sehun <twilivez@gmail.com>
2020-10-18 04:32:29 +0900

    [UPDATE] Singleton 및 DontDestroySingleton 수정

---
+++ Komorio/Singleton Tools/SingletonObject.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.IO;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+/// <summary>
+/// Convert this class to Singleton format.
+/// </summary>
+public class SingletonObject<T> : MonoBehaviour where T : MonoBehaviour {
+    private static T instance;
+    public static T Instance {
+        get {
+            if (instance == null) {
+                var obj = GameObject.FindObjectOfType<T>();
+
+                if (obj == null) {
+                    var prefabs = Resources.Load<T>($"Prefabs/{typeof(T).ToString()}");
+                    
+                    if (prefabs != null) {
+                        obj = prefabs;
+                        Instantiate(prefabs.gameObject, Vector2.zero, Quaternion.identity);
+                    }
+                    else {
+                        obj = new GameObject(typeof(T).ToString()).AddComponent<T>();
+
+                        var directoryPath = $"{Application.dataPath}/Resources/Prefabs";
+                        
+                        var prefabsFolder = new DirectoryInfo(directoryPath);
+                        if (prefabsFolder.Exists == false) {
+                            prefabsFolder.Create();
+                        }
+
+                        var prefabPath = $"{Application.dataPath}/Resources/Prefabs/{typeof(T).ToString()}.prefab";
+                        prefabPath = AssetDatabase.GenerateUniqueAssetPath(prefabPath);
+                        
+                        PrefabUtility.SaveAsPrefabAssetAndConnect(obj.gameObject, prefabPath, InteractionMode.UserAction);
+                    }
+                }
+
+                SceneManager.activeSceneChanged += (beforeScene, afterScene) => {
+                    if (beforeScene != afterScene) {
+                        instance = null;
+                        "Destroy Instance".Log();
+                    }
+                };
+                
+                instance = obj;
+            }
+
+            return instance;
+        }
+    }
+}
