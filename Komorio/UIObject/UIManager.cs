285c3707c0ffc85d336e9395d0bb59a8d0835d89
Jeon Sehun <twilivez@gmail.com>
2020-10-18 05:02:51 +0900

    [ADD] UIBase 및 UIManager 추가

---
+++ Komorio/UIObject/UIManager.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIManager : DontDestroySingletonObject<UIManager>
+{
+    public void OpenUI<T>(params object[] args) where T : UIBase {
+        SingletonObject<T>.Instance.OpenUI();
+    }
+    public void CloseUI<T>(params object[] args) where T : UIBase {
+        SingletonObject<T>.Instance.CloseUI();
+    }
+}
